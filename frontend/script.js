37376b98e11ff9b822436031cd1a94b032fe24b7
Mehwish Zehra <mehwishzehra12345@gmail.com>
2026-06-17 08:17:16 +0500

    frontend update

--- frontend/script.js
+++ frontend/script.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
             // Hit FastAPI Endpoint
-            const response = await fetch("http://127.0.0.1:8000/predict", {
+            const response = await fetch("https://mmehwishhh-spam-detector-api.hf.space/predict", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
